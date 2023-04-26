57ea78fcaf191bc37ddd47a9c5efcfd00cc0e1ba
quarlessam <quarlessam@gmail.com>
2023-04-25 21:57:15 -0400

    json test

--- server.js
+++ server.js
@@ -6,26 +6,43 @@ import {rpsls} from './lib/rpsls.js'
 const args = minimist(process.argv.slice(2))
 const port = args.port || 5000
 
-var app = express();
+var app = express()
+app.use(express.json())
 
 app.get('/app', function(req, res){
-   res.send("200 OK")
+  res.send("200 OK")
 })
 
 app.get('/app/rps', function(req, res){
-   res.send(rps())
+  res.send(rps())
 })
 
 app.get('/app/rpsls', function(req, res){
-   res.send(rpsls())
+  res.send(rpsls())
 })
 
 app.get('/app/rps/play', function(req, res){
-   res.send(rps(req.query.SHOT))
+  if (req.query.shot != null){
+    res.send(rps(req.query.shot))
+  } else if (res.json(req.body).shot != null){
+    res.send(rps(res.json(req.body).shot))
+  } else {
+    res.send(rps())
+  }
 })
 
 app.get('/app/rpsls/play', function(req, res){
-   res.send(rpsls(req.query.SHOT))
+  if (req.query.shot != null){
+    res.send(rpsls(req.query.shot))
+  } else if (res.json(req.body).shot != null){
+    res.send(rpsls(res.json(req.body).shot))
+  } else {
+    res.send(rpsls())
+  }
+})
+
+app.get('/app/rpsls/play', function(req, res){
+  res.send(rpsls(req.query.shot))
 })
 
 
